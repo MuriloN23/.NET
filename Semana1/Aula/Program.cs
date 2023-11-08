6620c36427a1583c023aede7eac63cbce09c3499
Murilo Novais <muhnovais@gmail.com>
2023-11-08 12:33:47 -0300

    commit atualização

---
+++ Semana1/Aula/Program.cs
@@ -0,0 +1,20 @@
+﻿#region tipos de dados
+
+int tipoInteiro;
+double tipoDouble;
+bool tipoBooleano;
+string tipoTexto;
+long tipoInteiroLong;
+
+
+tipoInteiro = int.MaxValue;
+Console.WriteLine(tipoInteiro);
+
+//tipoInteiroLong = 10;
+//tipoInteiro = long.Parse(tipoInteiroLong);
+
+tipoInteiroLong = long.MaxValue;
+Console.WriteLine(tipoInteiroLong);
+
+
+#endregion
