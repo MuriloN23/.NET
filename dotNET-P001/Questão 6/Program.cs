6c93fb63682dc6e6ea7bf5ebc3369d506ba301bb
Murilo Novais <muhnovais@gmail.com>
2023-11-17 17:07:05 -0300

    commit questao 6

---
+++ dotNET-P001/Questão 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Strings
+        string str1 = "Murilo";
+        string str2 = "Novais";
+
+        // Verificar se as strings são iguais
+        bool saoIguais = str1 == str2;
+
+        // Exibir o resultado
+        if (saoIguais)
+        {
+            Console.WriteLine("As strings são iguais.");
+        }
+        else
+        {
+            Console.WriteLine("As strings não são iguais.");
+        }
+    }
+}
