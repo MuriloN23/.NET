b80ae4dfc266bfaf5cbd94d24df7fbf93c167add
Murilo Novais <muhnovais@gmail.com>
2023-11-17 16:39:49 -0300

    commit questao 3

--- dotNET-P001/Questão 3/Program.cs
+++ dotNET-P001/Questão 3/Program.cs
@@ -4,13 +4,11 @@ class Program
 {
     static void Main()
     {
-        // Variável do tipo double com parte fracionária
+
         double numeroDouble = 10.75;
 
-        // Convertendo double para int (parte fracionária será truncada)
         int numeroInteiro = (int)numeroDouble;
 
-        // Exibindo os resultados
         Console.WriteLine($"Número Double: {numeroDouble}");
         Console.WriteLine($"Número Inteiro após conversão: {numeroInteiro}");
     }
