127f8ea83a8941ce56acac2c7ae892b1709f6cd6
Murilo Novais <muhnovais@gmail.com>
2023-11-29 11:09:04 -0300

    commit 1

---
+++ dotNET-AV/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace POGDevConsultorio
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CPF { get; set; }
+
+        public Pessoa(string nome, DateTime dataNascimento, string cpf)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            if (cpf.Length == 11)
+            {
+                CPF = cpf;
+            }
+            else
+            {
+                throw new ArgumentException("O CPF deve ter 11 dígitos.");
+            }
+        }
+    }
+}
